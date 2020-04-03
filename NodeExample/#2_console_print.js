5500bd33031978cc97263a40f8731bbafde9a949
daasol <dasol806@kpu.ac.kr>
2020-04-03 16:43:10 +0900

    console_test

--- NodeExample/#2_console_print.js
+++ NodeExample/#2_console_print.js
@@ -7,3 +7,6 @@ var person = {
 console.log('자바스크립트 객체.  %j', person);
 
 console.dir(person);
+
+console.log('file name : %s', __filename);
+console.log('path : %s', __dirname);
--- NodeExample/#3_console_time.js
+++ NodeExample/#3_console_time.js
@@ -1,17 +1,5 @@
-console.log('안녕하세요.');
 
-console.log('숫자 %d', 10);
-console.log('문자열. %s', '안녕');
 
-var person = {
-    name : 'dasol',
-    age : 25
-    
-};
-
-console.log('자바스크립트 객체.  %j', person);
-
-console.dir(person);
 
 console.time('duration_time');
 //key값을 괄호 안에 넣어주고, key값으로 닫아준다.
@@ -22,3 +10,6 @@ for(var i=0; i<10000; i++){
     result+=i;
 }
 console.timeEnd('duration_time');
+
+console.log('file name : %s', __filename);
+console.log('path : %s', __dirname);
