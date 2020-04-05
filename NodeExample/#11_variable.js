ab1ca85a168d40d08f1eaca6c13f0a6310159259
daasol <dasol806@kpu.ac.kr>
2020-04-05 21:25:22 +0900

    node&javascript

---
+++ NodeExample/#11_variable.js
@@ -0,0 +1,7 @@
+var name;
+console.log('name : '+name);
+var age = 20;
+console.log('age : ' +age);
+
+name ='Daisy';
+console.log('name : '+name);
---
+++ NodeExample/#12_object_test.js
@@ -0,0 +1,11 @@
+var obj = {};
+
+obj['name'] = 'Daisy';
+//name이라는 속성이 obj 객체에 추가된 것
+
+obj['age']=20;
+
+console.log('name : '+obj.name);
+//(.)점으로 객체의 속성을 접근
+
+console.log('age : '+obj['age']);
