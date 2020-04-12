5de7cf14a493ad203c24094721ad4b65a95c7b7b
daasol <dasol806@kpu.ac.kr>
2020-04-12 21:33:38 +0900

    httpServer, expressServer

--- WebServer_test/#1 http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var server = http.createServer();
-
-var port = 3000;
-server.listen(port, function(){
-    console.log('web server Strat! : '+port);
-});
-//클라이언트 요청 대기 (포트번호, callback함수)
