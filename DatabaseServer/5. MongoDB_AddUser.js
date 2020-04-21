498928de48a697e49ff2f055d97d8c3ca81d3fbd
daasol <dasol806@kpu.ac.kr>
2020-04-21 12:35:37 +0900

    addUser

--- DatabaseServer/5. MongoDB_AddUser.js
+++ DatabaseServer/5. MongoDB_AddUser.js
@@ -66,12 +66,50 @@ router.route('/process/login2').post(function(req, res){
     }else{
         console.log('67 : 에러발생');
         res.writeHead(200, {"Content-Type":'text/html;charset=UTF-8'});
-        res.write('<h1>사용자 데이터 조회 안됨</h1>');
+        res.write('<h1>데이터베이스 연결 안됨</h1>');
     }
 });
 
+router.route('/process/addUser').post(function(req,res){
+    console.log('/process/addUser 라우팅 함수 호출됨');
+    
+    var paramId = req.body.id;
+    var paramPassword = req.body.password;
+    var paramName = req.body.name;
+    
+    console.log('paramId: '+paramId+', paramPassword:'+paramPassword+ ', paramName:'+paramName);
+    if(db){
+        addUser(db,paramId, paramPassword, paramName, function(err, result){
+            if(err){
+                console.log('에러 발생');
+                res.writeHead(200, {"Content-Type":"text/html;charset=UTF-8"});
+                res.write('<h1>에러 발생</h1>');
+                res.end();
+            }
+            if(result){
+                console.dir(result);
+                
+                res.writeHead(200, {"Content-Type":"text/html;charset=UTF-8"});
+                res.write('<h1>사용자 추가 성공</h1>');
+                res.write('<div><p>사용자 : '+paramName+'</div></p>');
+                res.end();
+            }else{
+                console.log('97 : 에러발생');
+                res.writeHead(200, {"Content-Type":'text/html;charset=UTF-8'});
+                res.write('<h1>사용자 데이터 추가 안됨</h1>');
+            }
+        });
+    } else{
+        console.log('103 : 에러발생');
+        res.writeHead(200, {"Content-Type":'text/html;charset=UTF-8'});
+        res.write('<h1>데이터베이스 연결 안됨</h1>');
+    }
+});
+
+
 app.use('/', router);
 
+//사용자 찾기
 var authUser = function(db, id, password, callback){
     console.log('authUser 호출 ...');
     
@@ -88,7 +126,34 @@ var authUser = function(db, id, password, callback){
             callback(null, null);
         }
     });
-}
+};
+
+var addUser = function(db, id, password, name, callback){
+    console.log('addUser 호출 ... ');
+    console.log('>> id : '+id+' password : '+password+', name : '+name);
+    
+    var user = db.collection('user');
+    
+    user.insertMany([{"id":id, "password":password, "name":name}], function(err, result){
+        if(err){
+            callback(err, null);
+            return;
+        }
+        if(result.insertedCount>0){
+            console.log('사용자 추가 완료 ... : '+result.insertedCount);
+            callback(null, result);
+            
+        }else{
+            console.log('추가된 레코드가 없음');
+            callbacl(null, cnull);
+            
+        }
+        
+    });
+    //여러개를 추가할 수 있는 메소드이므로 배열로 객체를 넣을 수 있음
+    
+};
+
 
 var errorHandler = expressErrorHandler({
     static:{'404':'./public/404.html'}
