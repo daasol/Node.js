eaa41df31154538dd9d3af6ffe6df3f30fcd50d0
daasol <dasol806@kpu.ac.kr>
2020-05-02 22:08:27 +0900

    Require_module

---
+++ ModuleExample/(7)Require.js
@@ -0,0 +1,18 @@
+var require = function(paht){
+    var exports = {};
+    
+    exports.getUser = function(){
+          return {id:'daisy', name:'데이지'};
+    };
+    
+    exports.group = {id:'group1', name:'직장동료'};
+    
+    return exports;
+};
+
+var user =require('...');
+
+function showUser(){
+    return user.getUser().name+','+user.group.name;
+}
+console.log('사용자 정보 ... '+showUser());
