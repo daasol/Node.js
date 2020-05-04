7ad6e56f5a9e6d5b769900e14334f2f79c89775a
daasol <dasol806@kpu.ac.kr>
2020-05-04 20:47:31 +0900

    BasicConde

---
+++ ModuleExample/(14)DBmodule.js
@@ -0,0 +1,11 @@
+var exporess = require('express');
+var http = require('http');
+var static = require('serve-static');
+var path =require('path');
+var bodyParser = require('body-parser');
+var cookieParser =require('cookie-parser');
+var express = require('express-sesiion');
+var expressErrorHandler = require('express-error-handler');
+// ~ express를 사용하여 처리하는 기본 모듈
+
+
